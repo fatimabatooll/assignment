bf986117447c274a1fc39e8000f71cbf8f42c414
fatimabatool <fatima.batool@xloopdigital.com>
2023-04-13 14:08:43 -0700

    insert read delete

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import Header from './Components/Header';
 
 function App() {
   const [data, setData] = useState([])
+  const handleDelete = (title) => {
+   // console.log(title)
+   let filterData = data.filter((e) => e.title != title);
+   setData(filterData)
+  }
+  
   return (
     <div className="App">
       <Header setData={setData} data={data}/>
       {data.map((e) => {
         return(
-          <Goals title={e.title} info={e.info}/>
+          <Goals title={e.title} info={e.info} handleDelete={handleDelete}/>
         )
       })}
     </div>
--- src/Components/Goals.js
+++ src/Components/Goals.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Goals = ({title, info}) => {
+const Goals = ({title, info, handleDelete}) => {
   return (
     <div>
     <h1>{title}</h1>
     <p>{info}</p>
+    <button onClick={() => {handleDelete(title)} }>Delete</button>
     </div>
   )
 }
